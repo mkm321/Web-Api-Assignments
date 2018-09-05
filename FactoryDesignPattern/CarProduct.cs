ec011fcece8d5e888909e596bd4befa65623f3a0
Mohit Mehndiratta <mmehndiratta@tavisca.com>
2018-09-05 13:49:58 +0530

    Factory Pattern Assignments with Switch-Case

---
+++ FactoryDesignPattern/CarProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    class CarProduct : IProduct
+    {
+        public void Book()
+        {
+            Console.WriteLine("Booked");
+        }
+
+        public string GetTypeOfProduct()
+        {
+            return "Car";
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("saved");
+        }
+    }
+}
---
+++ FactoryDesignPattern/FactoryMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    class FactoryMethod
+    {
+        public IProduct GetProduct(string product)
+        {
+            switch (product)
+            {
+                case "car": return new CarProduct();
+                    break;
+                case "hotel": return new HotelProduct();
+                    break;
+                case "activity": return new ActivityProduct();
+                    break;
+                default: return new AirProduct();
+                    break;
+            }
+        }
+    }
+}
