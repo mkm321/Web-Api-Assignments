6ead14175b221e021cb798610be994f22c173064
Mohit Mehndiratta <mmehndiratta@tavisca.com>
2018-09-07 19:12:23 +0530

    Added Sql Database with Database Script File

--- FactoryDesignPattern/FactoryDesignPattern/FileDatabase.cs
+++ FactoryDesignPattern/FactoryDesignPattern/FileDatabase.cs
@@ -12,6 +12,13 @@ namespace FactoryDesignPattern
         Logger logs = Logger.getInstance();
         public void AddProduct(string data, string operation)
         {
+            string[] result = data.Split(',');
+            foreach (string response in result)
+            {
+                string[] keyPair = response.Split(':');
+                Console.WriteLine(keyPair[0] + ": " + keyPair[1]);
+            }
+
             FileStream fs = new FileStream("C:\\saveFile.txt", FileMode.Append, FileAccess.Write);
 
             StreamWriter sw = new StreamWriter(fs);
--- FactoryDesignPattern/FactoryDesignPattern/SQLDatabase.cs
+++ FactoryDesignPattern/FactoryDesignPattern/SQLDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,30 @@ namespace FactoryDesignPattern
         Logger logs = Logger.getInstance();
         public void AddProduct(string data, string operation)
         {
+            string[] result = data.Split(',');
+            string query = "";
+            string queryValue = "";
+            foreach(string response in result)
+            {
+                string[] keyPair = response.Split(':');
+                Console.WriteLine(keyPair[0] + ": " + keyPair[1]);
+                query += "@" + keyPair[0] + ",";
+                queryValue += keyPair[1] + ",";
+            }
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = "Data Source=TAVDESK083;Initial Catalog=Travel;Integrated Security=True";
+            query = query.Remove(query.Length - 1, 1);
+            string con = "insert into " + operation + " values(" + query + ")";
+            sqlConnection.Open();
+            SqlCommand sqlCommand = new SqlCommand(con,sqlConnection);
+            string[] dynamicColumn = query.Split(',');
+            string[] dynamicColumnValue = queryValue.Split(',');
+            for(int index = 0; index < dynamicColumn.Length; index++)
+            {
+                sqlCommand.Parameters.AddWithValue(dynamicColumn[index], dynamicColumnValue[index]);
+            }
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
             logs.loggingDetails("Saving into database");
 
         }
